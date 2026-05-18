2889925b01b705e90014bac80850475e7f1c1e06
babos-2006 <austinjillo14@gmail.com>
2026-05-18 10:49:01 +0300

    Update ai_engine.py

--- ai_engine.py
+++ ai_engine.py
@@ -18,7 +18,7 @@ API_KEY = os.getenv("GEMINI_API_KEY")
 
 genai.configure(api_key=API_KEY)
 
-model = genai.GenerativeModel("gemini-2.0-flash")
+model = genai.GenerativeModel("gemini-pro")
 
 # =========================
 # MEMORY
